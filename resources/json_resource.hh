cb3fae5cacb174e8d261c7491690de0cea097b1e
Matt Langford <matthew.langford95@gmail.com>
2018-05-28 22:14:02 -0400

    Add json resource

---
+++ resources/json_resource.hh
@@ -0,0 +1,36 @@
+#include "resources/abstract_resource.hh"
+#include "json/json.hh"
+
+namespace resources
+{
+
+class json_resource : public abstract_resource
+{
+public: ///////////////////////////////////////////////////////////////////////
+    ///
+    ///
+    ///
+    virtual ~json_resource() = default;
+
+public: ///////////////////////////////////////////////////////////////////////
+    ///
+    ///
+    ///
+    inline virtual const std::string& get_resource_identifier() const = 0;
+
+    ///
+    /// Get a json object from the resource, this will be converted to a string and passed along
+    ///
+    virtual json::json get_json_resource() = 0;
+
+    ///
+    ///
+    ///
+    inline const std::string& get_resource_type() override const { return "text/json"; }
+
+    ///
+    ///
+    ///
+    inline std::string get_resource() override { return get_json_resource().get_value_as_string(); }
+}
+}
